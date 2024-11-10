cf0eea5d5a7184e2ea1e1332a3107e2d5345159f
jjjung0921 <earthenergy-1128@naver.com>
2024-11-11 01:45:44 +0900

    [Design] 웹 반응형 디자인 수정

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,32 +1,31 @@
-import React, { useState } from 'react'
-import '../styles/header.scss'
-import Opening from '../img/Opening.svg'
-import Mail from '../img/Mail.svg'
+import React, { useState } from "react";
+import "../styles/header.scss";
+import Opening from "../assets/img/Opening.svg";
+import Mail from "../assets/img/Mail.svg";
 
 const Header = () => {
-    const [isOpen, setIsOpen] = useState(false);
-    const toggleList = () => {
-        setIsOpen(!isOpen);
-    }
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleList = () => {
+    setIsOpen(!isOpen);
+  };
   return (
-    <div className='Header_Container'>
-      <img src={Opening} className='Logo' alt='Logo'></img>
-      <div className='Nav' onClick={toggleList}>
-        <img src={Mail} className='Nav_ico' alt='Mail'></img>
-      </div>
-      {isOpen && (
-        <div className='Nav_list'> 
-            <ul className='List_drop'>
-                <li>Login</li>
-                <li>Join</li>
-                <li>User Info</li>
-                <li>My Letter Box</li>
-            </ul>
+      <div className="Header_Container">
+        <img src={Opening} className="Logo" alt="Logo"></img>
+        <div className="Nav" onClick={toggleList}>
+          <img src={Mail} className="Nav_ico" alt="Mail"></img>
         </div>
-        
-      )}
-    </div>
-  )
-}
+        {isOpen && (
+          <div className="Nav_list">
+            <ul className="List_drop">
+              <li>Login</li>
+              <li>Join</li>
+              <li>User Info</li>
+              <li>My Letter Box</li>
+            </ul>
+          </div>
+        )}
+      </div>
+  );
+};
 
-export default Header
+export default Header;
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,21 +1,26 @@
-import React from 'react'
-import Header from '../components/Header'
-import Button from '../components/Button'
-import '../styles/app.scss'
-import Heart from '../img/Heart.svg'
+import React from "react";
+import Header from "../components/Header";
+import Button from "../components/Button";
+import "../styles/app.scss";
+import Heart from "../assets/img/Heart.svg";
+import { Link } from "react-router-dom";
 
 const Main = () => {
   return (
     <>
-    <Header/>
-    <div className='Main_Container'>
+      <Header />
+      <div className="Main_Container">
         <p>어김없이 돌아온 연말</p>
-        <p><img src={Heart}></img>따뜻한 마음을 담아,</p>
+        <p>
+          <img src={Heart}></img>따뜻한 마음을 담아,
+        </p>
         <p>소중한 사람에게 감사 카드를 전해보세요.</p>
-    </div>
-    <Button text='마음 전하러 가기'/>
+      </div>
+      <Link to={"/write"}>
+        <Button text="마음 전하러 가기" />
+      </Link>
     </>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
