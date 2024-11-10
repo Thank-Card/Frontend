95a3aa5e6c66b5128b752f1675bef3db46be1a50
Heeju1016 <huiju232@gmail.comcd >
2024-11-10 20:37:21 +0900

    start

---
+++ src/components/Header.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+import '../styles/header.scss'
+import Opening from '../img/Opening.svg'
+import Mail from '../img/Mail.svg'
+
+const Header = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const toggleList = () => {
+        setIsOpen(!isOpen);
+    }
+  return (
+    <div className='Header_Container'>
+      <img src={Opening} className='Logo' alt='Logo'></img>
+      <div className='Nav' onClick={toggleList}>
+        <img src={Mail} className='Nav_ico' alt='Mail'></img>
+      </div>
+      {isOpen && (
+        <div className='Nav_list'> 
+            <ul className='List_drop'>
+                <li>Login</li>
+                <li>Join</li>
+                <li>User Info</li>
+                <li>My Letter Box</li>
+            </ul>
+        </div>
+        
+      )}
+    </div>
+  )
+}
+
+export default Header
---
+++ src/pages/Main.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Header from '../components/Header'
+import Button from '../components/Button'
+import '../styles/app.scss'
+import Heart from '../img/Heart.svg'
+
+const Main = () => {
+  return (
+    <>
+    <Header/>
+    <div className='Main_Container'>
+        <p>어김없이 돌아온 연말</p>
+        <p><img src={Heart}></img>따뜻한 마음을 담아,</p>
+        <p>소중한 사람에게 감사 카드를 전해보세요.</p>
+    </div>
+    <Button text='마음 전하러 가기'/>
+    </>
+  )
+}
+
+export default Main
