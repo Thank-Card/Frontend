e1ee7c8f60b049cdaf4197d23217465137a1a8b3
jjjung0921 <earthenergy-1128@naver.com>
2024-11-16 01:13:25 +0900

    [Feat] 로그인 시 쿠키 저장

--- src/pages/Join.jsx
+++ src/pages/Join.jsx
@@ -1,8 +1,9 @@
-import React, { useEffect, useState } from 'react'
-import '../styles/Join.scss'
+import React, { useEffect, useState } from 'react';
+import '../styles/Join.scss';
 import Header from '../components/Header';
 import api from '@/api/axios';
 import { useNavigate } from 'react-router-dom';
+import { Cookies } from 'react-cookie';
 
 const Join = () => {
   const [id, setId] = useState("");
@@ -11,57 +12,67 @@ const Join = () => {
   const [checkPassword, setCheckPassword] = useState("");
   const [isJoinDisabled, setIsJoinDisabled] = useState(true);
   const navigate = useNavigate();
-  
-  const handleIdChange = (e) => {setId(e.target.value)};
-  const handleNameChange = (e) => {setName(e.target.value)};
-  const handlePasswordChange = (e) => {setPassword(e.target.value)};
-  const handleCheckPasswordChange = (e) => {setCheckPassword(e.target.value)};
+  const cookies = new Cookies(); // 쿠키 인스턴스 생성
 
-  useEffect(()=>{
-    if(password!=checkPassword || password===""){ //비밀번호 일치하지 않을 때 코드 작성해주세요.
-      if(id==="" || name==="")
+  const handleIdChange = (e) => { setId(e.target.value); };
+  const handleNameChange = (e) => { setName(e.target.value); };
+  const handlePasswordChange = (e) => { setPassword(e.target.value); };
+  const handleCheckPasswordChange = (e) => { setCheckPassword(e.target.value); };
+
+  useEffect(() => {
+    if (password !== checkPassword || password === "") {
+      if (id === "" || name === "")
         setIsJoinDisabled(true);
-    } else{
-        setIsJoinDisabled(false);
+    } else {
+      setIsJoinDisabled(false);
     }
-  },[password, checkPassword, id, name]);
+  }, [password, checkPassword, id, name]);
 
-  const joinAccount = async(event) => {
+  const joinAccount = async (event) => {
     event.preventDefault();
-    //console.log(typeof(id), typeof(name), typeof(password));
     const userData = {
-        loginId: id,
-        name: name,
-        password: password,
-      };
-    console.log(userData);
+      loginId: id,
+      name: name,
+      password: password,
+    };
+    
     try {
       const response = await api.post('/api/users/join', userData, {
         headers: {
           "Content-Type": "application/json",
         },
       });
-      // console.log(response);
-      if(response.status === 200 || response.status === 201){
+
+      if (response.status === 200 || response.status === 201) {
+        // 서버에서 JWT 토큰을 받는다고 가정합니다.
+        const token = response.data.token; // 서버에서 반환하는 토큰
+        if (token) {
+          // 쿠키에 토큰 저장
+          cookies.set("token", `JWT ${token}`, {
+            path: "/",
+            sameSite: "strict",
+            secure: true, // HTTPS에서만 전송되도록 설정
+            httpOnly: true // JavaScript에서 접근할 수 없도록 설정
+          });
+        }
         alert('회원가입이 성공적으로 진행되었습니다.');
         navigate('/login');
       }
-      return response.data;
     } catch (error) {
       console.error("API 요청 오류:", error.response.data);
-      if(error.response.data === "User already exist")
+      if (error.response.data === "User already exist")
         alert("이미 존재하는 회원입니다.");
-    }    
+    }
   }
 
   return (
     <div className='Join_Container'> 
-      <Header/>
+      <Header />
       <div className='Join_box'>
-        <div className='Id_box'><input className='Id' type='text' placeholder='Id' onChange={handleIdChange}></input></div>
-        <div className='Name_box'><input className='Name' type='text' placeholder='Name' onChange={handleNameChange}></input></div>
-        <div className='Pw_box'><input className='Pw' type='password' placeholder='Password' onChange={handlePasswordChange}></input></div>
-        <div className='Pwc_box'><input className='Pwc' type='text' placeholder='Password Check' onChange={handleCheckPasswordChange}></input></div>
+        <div className='Id_box'><input className='Id' type='text' placeholder='Id' onChange={handleIdChange} /></div>
+        <div className='Name_box'><input className='Name' type='text' placeholder='Name' onChange={handleNameChange} /></div>
+        <div className='Pw_box'><input className='Pw' type='password' placeholder='Password' onChange={handlePasswordChange} /></div>
+        <div className='Pwc_box'><input className='Pwc' type='password' placeholder='Password Check' onChange={handleCheckPasswordChange} /></div>
       </div>
       <div className='Login_btn_box'>
         <form onSubmit={joinAccount}>
@@ -69,7 +80,7 @@ const Join = () => {
         </form>
       </div>
     </div>
-  )
+  );
 }
 
-export default Join
+export default Join;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,51 +1,74 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import '../styles/Login.scss';
-import Header from '../components/Header';
-import Heart1 from '../assets/img/Heart1.png'
+import React, { useState } from "react";
+import axios from "@/api/axios";
+import "../styles/Login.scss";
+import Header from "../components/Header";
+import Heart1 from "../assets/img/Heart1.png";
+import { setCookie } from "@/api/Cookies";
 
 const Login = () => {
-    const [id, setId] = useState("");
-    const [pw, setPw] = useState("");
+  const [id, setId] = useState("");
+  const [pw, setPw] = useState("");
 
-    const postFetchLogin = async () => {
-        try {
-            const response = await axios.post('http://3.38.151.193:8080/api/users/login', {
-                loginId: id,
-                password: pw
-            });
-            console.log(id,pw)
-            console.log(response)
-        }catch(error) {
-            console.log(id,pw);
-            console.log(error);
+  const postFetchLogin = async (event) => {
+    event.preventDefault(); // 기본 폼 제출 동작 방지
+    //console.log("Try Login");
+    try {
+      const response = await axios.post(
+        "/api/users/login",
+        { loginId: id, password: pw },
+        {
+          withCredentials: true,
         }
+      );
+      if (response.headers.getAuthorization()) {
+        console.log("token 저장");
+        setCookie("token", `${response.headers.getAuthorization()}`, {
+          path: "/",
+          sameSite: "strict",
+        });
+      }
+      console.log("로그인 성공:", response);
+    } catch (error) {
+      console.error("API 호출 에러", error.response ? error.response.data : error.message);
     }
-    
-    function handleId(e) {
-        setId(e.target.value);
-        console.log(id)
-    }
-    function handlePw(e) {
-        setPw(e.target.value);
-    }
+  };
 
-    return (
-        <div className='Login_Container'>
-        <Header/>
-        <div className='Login_box'>
-            <div className='Id_box'><input className='Id' value={id} type='text' placeholder='Id' onChange={(e) => handleId(e)}/></div>
-            <div className='Pw_box'><input className='Pw' value={pw} type='text' placeholder='Password' onChange={(e) => handlePw(e)}/></div>
+  return (
+    <div className="Login_Container">
+      <Header />
+      <div className="Login_box">
+        <div className="Id_box">
+          <input
+            className="Id"
+            value={id}
+            type="text"
+            placeholder="Id"
+            onChange={(e) => setId(e.target.value)}
+          />
         </div>
-        <div className='Join_box'>
-            <img src={Heart1}></img>
-            <div className='Join_btn'>join</div>
+        <div className="Pw_box">
+          <input
+            className="Pw"
+            value={pw}
+            type="password"
+            placeholder="Password"
+            onChange={(e) => setPw(e.target.value)}
+          />
         </div>
-        <div className='Login_btn_box'>
-            <button type='button' text='Login' className='Login_btn' onClick={postFetchLogin}>Login</button>
-        </div>
-        </div>
-    )
-}
+      </div>
+      <div className="Join_box">
+        <img src={Heart1} alt="Heart" />
+        <div className="Join_btn">join</div>
+      </div>
+      <div className="Login_btn_box">
+        <form onSubmit={postFetchLogin}>
+          <button type="submit" className="Login_btn">
+            Login
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
