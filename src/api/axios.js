aef1550683eb9213a0d21700e6edcbf66e731b89
jjjung0921 <earthenergy-1128@naver.com>
2024-11-16 11:29:46 +0900

    [Feat] axios 토큰 호출 기능 구현

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,17 +1,15 @@
 import axios from "axios";
 
+const getToken = localStorage.getItem('Token');
+
 const api = axios.create({
-    baseURL: process.env.REACT_APP_BASE_URL
+    baseURL: process.env.REACT_APP_BASE_URL,
+    headers: {
+        Authorization: getToken,
+    },
 });
 
-// api.interceptors.request.use((config) => {
-//     const token = localStorage.getItem('jwtToken'); // 로컬 스토리지에서 토큰 가져오기
-//     if (token) {
-//         config.headers['Authorization'] = `Bearer ${token}`;
-//     }
-//     return config;
-// }, (error) => {
-//     return Promise.reject(error);
-// });
+
+
 
 export default api;
