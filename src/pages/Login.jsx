69edcd06bfc42944a49b4a631c700e735a79de2d
jjjung0921 <earthenergy-1128@naver.com>
2024-11-16 11:52:13 +0900

    [Feat] User Info 완료

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -22,10 +22,6 @@ const Login = () => {
         }
       );
       if (response.headers.getAuthorization()) {
-<<<<<<< HEAD
-=======
-        console.log("token 저장: ", response.headers.getAuthorization());
->>>>>>> 120e52668131095745f6911cad08ec1a1f8ca769
         localStorage.setItem("Token", response.headers.getAuthorization());
       }
       console.log("로그인 성공:", response);
