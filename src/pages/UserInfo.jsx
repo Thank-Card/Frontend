40afef5fcb02bdb8a4a1a20d9e5229fac84cde7b
2021110765@dgu.ac.kr <2021110765@dgu.ac.kr>
2024-11-16 13:24:00 +0900

    [Feat]로그아웃 기능

--- src/pages/UserInfo.jsx
+++ src/pages/UserInfo.jsx
@@ -2,14 +2,18 @@ import React, { useEffect, useState } from "react";
 import Header from "@components/Header";
 import "@styles/UserInfo.scss";
 import api from "@/api/axios"; // axios를 통해 API 호출을 관리합니다.
+import { useNavigate } from "react-router-dom";
 
 const UserInfo = () => {
   const [userInfo, setUserInfo] = useState({ loginId: "", name: "" });
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchUserInfo = async () => {
       try {
-        const response = await api.get("/api/users/user-info");
+        const response = await api.get("/api/users/user-info", {
+          withCredentials: true,
+        });
         const { loginId, name } = response.data;
         setUserInfo({ loginId, name });
       } catch (error) {
@@ -20,6 +24,24 @@ const UserInfo = () => {
     fetchUserInfo();
   }, []);
 
+  const handleLogout = async (event) => {
+    event.preventDefault();
+    try {
+      await api.post("/api/users/logout", null, {
+        withCredentials: true,
+      });
+      localStorage.removeItem("Token"); // 저장된 토큰 삭제
+      console.log("로그아웃 성공");
+      navigate("/login", { replace: true }); // 로그인 페이지로 이동
+      window.location.reload(); // 페이지 강제 새로고침
+    } catch (error) {
+      console.error(
+        "로그아웃 실패:",
+        error.response ? error.response.data : error.message
+      );
+    }
+  };
+
   return (
     <>
       <Header />
@@ -27,13 +49,21 @@ const UserInfo = () => {
       <h2 className="title">User Info</h2>
 
       <div className="user-info">
-        <span className="label">Id</span>
+        <span className="label">Id </span>
         <span className="value">{userInfo.loginId}</span>
       </div>
 
       <div className="user-info">
-        <span className="label">Name</span>
+        <span className="label">Name </span>
         <span className="value">{userInfo.name}</span>
+      </div>
+
+      <div className="Logout_btn_box">
+        <form onSubmit={handleLogout}>
+          <button type="submit" className="Logout_btn">
+            Logout
+          </button>
+        </form>
       </div>
     </>
   );
